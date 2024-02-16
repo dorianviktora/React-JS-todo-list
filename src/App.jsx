512cad1f1b46e89399a75d41713e4a0e9e2cb211
dorianviktora <viktoradorian@gmail.com>
2024-02-16 17:34:22 +0100

    adding items, checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,32 @@ import { useState } from "react"
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
 
   function handleSubmit(e) {
     e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(),
+          title: newItem,
+          completed: false}
+      ]
+    })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id == id) {
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
   }
 
   return (
@@ -23,15 +46,22 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
+
+        {todos.map(todo => {
+          return(
+          <li key={todo.id}>
           <label>
-            <input type="checkbox" />
-            Item 1
+            <input type="checkbox"
+             checked={todo.completed}
+             onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+            {todo.title}
           </label>
           <button className="btn btn-danger">
             Delete
           </button>
-        </li>
+        </li>)
+        })}
+
       </ul>
     </>
   )
