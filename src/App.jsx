1c726b12d638ae80a9c9fbc74d7768d161c9ceb2
dorianviktora <viktoradorian@gmail.com>
2024-02-16 18:01:17 +0100

    delete button

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,12 @@ export default function App() {
     })
   }
 
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+   })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -56,7 +62,7 @@ export default function App() {
              onChange={e => toggleTodo(todo.id, e.target.checked)}/>
             {todo.title}
           </label>
-          <button className="btn btn-danger">
+          <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">
             Delete
           </button>
         </li>)
